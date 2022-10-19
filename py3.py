8792648c6ffdb9512a629286910edcd962d76098
PHMac20XX2 <phmestudos@gmail.com>
2022-10-19 19:17:14 -0300

    py3

---
+++ py3.py
@@ -0,0 +1,21 @@
+n1 = 5
+n2 = 10
+
+print ( 100 * '-')
+
+if n1 >= 5:
+    print ('Valor da Adição: ', n1 + n2)
+if n1 >= 5:
+    print ('Valor da Subtração: ', n1 - n2)
+if n2 >= 10:
+    print ('Valor da Multiplicação: ', n1 * n2)
+if n1 >= 5:
+    print ('Valor da Exponenciação: ', n1 ** n2)
+if n2 >=10:
+    print ('Valor da Divisão: ', n1 / n2)
+if n1 >= 5:
+    print ('Valor do Resto da divisão: ', n1 % n2)
+else:
+    print ("Operador inexistente")
+    
+print ( 100 * '-')
