de547f6f8dece4a6eb7639bf47fa45388df19504
PHMac20XX2 <phmestudos@gmail.com>
2022-10-19 13:54:06 -0300

    py2

--- py2.py
+++ py2.py
@@ -1,8 +1,9 @@
 a = 41
 b = 2
 
+
 if a >= 0:
-    print ('Positivo')
+    print ('Positivo') #Caso o valor seja positivo (maior que 0) exibir POSITIVO
 else:
-    print ('Negativo')
+    print ('Negativo') #Caso o valor seja negativo (menor que 0) exibir NEGATIVO
    
