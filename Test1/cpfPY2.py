58ebdcfab252f7fe69228a5b4169513024cfbc2d
PHMac20XX2 <phmestudos@gmail.com>
2022-10-19 18:41:36 -0300

    pyATT1

---
+++ Test1/cpfPY2.py
@@ -0,0 +1,15 @@
+"""
+cadastro - cpf
+"""
+
+#Nome
+print('Pedro', type('Pedro'))
+
+#Idade
+print(18, type(18))
+
+#Altura
+print(1.91, type(1.91))
+
+#VerificaIdade
+print(20 > 18, type(20 > 18))
--- py2.py
+++ py2.py
@@ -1,9 +1,19 @@
-a = 41
-b = 2
-
+a = 544
+b = 398
+c = 400
+d = 25
+e = 'Pedro'
 
 if a >= 0:
     print ('Positivo') #Caso o valor seja positivo (maior que 0) exibir POSITIVO
 else:
     print ('Negativo') #Caso o valor seja negativo (menor que 0) exibir NEGATIVO
-   
+
+#Printando CPF
+print (a, b, c, sep='.', end='-')
+
+#Type Casting
+if d == 25:
+    print(d, type(d), bool(d))
+else:
+    print ("Errado")       
