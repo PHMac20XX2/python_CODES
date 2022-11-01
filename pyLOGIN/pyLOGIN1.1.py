4620f73295062d52f5a1632c8ffb4520dc065673
PHMac20XX2 <phmestudos@gmail.com>
2022-11-01 17:33:50 -0300

    pyLOGIN 1.1

---
+++ pyLOGIN/pyLOGIN1.1.py
@@ -0,0 +1,10 @@
+nome = input("Nome de Usuário")
+senha = input("Senha de Usuário") 
+
+nome_bd = "Pedro"
+senha_bd = "040102004"
+
+if nome_bd == nome and senha_bd == senha:
+   print("Login Efetuado...")
+else:
+    print("Credenciais erradas!")
