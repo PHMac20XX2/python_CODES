27c72a214fd8f92a665509d555aa3426992438b7
PHMac20XX2 <phmestudos@gmail.com>
2022-11-01 15:52:53 -0300

    pyCALC1.0

---
+++ pyCALC/pyCALC.py
@@ -0,0 +1,30 @@
+
+print ("CALCULADORA")
+
+print(100 * '-')
+
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+
+ad1 = n1 + n2
+mul1 = n1 * n2
+sub1 = n1 - n2 
+div1 = n1 / n2
+
+opcao = int(input('''
+[1] Adição
+[2] Multiplicação
+[3] Subtração
+[4] Divisão
+Escolha uma operação:'''))
+
+if opcao == 1:
+    print(ad1)
+elif opcao == 2:
+    print (mul1)
+elif opcao == 3:
+    print (sub1)
+elif opcao == 4:
+    print (div1)
+else:
+    print ("Operador invalido")
